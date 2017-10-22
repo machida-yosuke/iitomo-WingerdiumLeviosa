3d4354b8055c3bd01c0dafe083bf74afe0ca65a8
machida-yosuke <machida-yosuke@kayac.com>
2017-10-22 18:47:52 +0900

    onseiireta

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,32 @@
-import Sample from './lib/Sample';
-import $ from 'jquery';
+import {Howl, Howler} from 'howler';
+const btn = document.getElementsByClassName('js-record-btn')[0];
+const content = document.getElementsByClassName('js-content')[0];
+const speech = new webkitSpeechRecognition();
 
-const sample = new Sample({
-    name: 'world'
+const sound = new Howl({
+  src: ['sound/stop.wav']
+});
+const yarimasita = new Howl({
+  src: ['sound/yarimasita.wav']
 });
 
-$('.wrapper').on('click', () => {
-    console.log(`hello, ${sample.name}.`);
-});
+
+speech.lang = "ja";
+btn.addEventListener('click',function(e){
+  speech.start();
+},false);
+
+speech.addEventListener( 'result' , function( e ) {
+	const quote = e.results[0][0].transcript;
+	if(quote == 'ウィンガーディアムレビオーサ' ){
+		content.innerHTML='スカート上がる';
+    yarimasita.play();
+		return;
+	}else if(quote == 'ウィンガーディアムレビオサー' ){
+		content.innerHTML='ちがう。あなたのはウィンガーディアムレビオサー';
+    sound.play();
+		return;
+	}else{
+		content.innerHTML=quote;
+	}
+},false);
